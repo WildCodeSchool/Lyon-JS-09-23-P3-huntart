da5fa5242ed96e6cf0746535bdc142bf3ec0d1f7
techmefr <compigni.gaetan@gmail.com>
2023-12-14 11:27:03 +0100

    create the database

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,4 +1,25 @@
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null
-);
+CREATE DATABASE IF NOT EXISTS `artAndGo`
+CHARACTER SET utf8mb4 COLLATE
+utf8mb4_unicode_ci;
+USE artAndGo;
+
+CREATE TABLE `streetArt` (
+  `id` int PRIMARY KEY,
+  `name` varchar(80),
+  `linkImage` varchar(255)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `streetArt_Theme` (
+  `id` int PRIMARY KEY,
+  `streetArt_id` int,
+  `theme_id` int
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `theme` (
+  `id` int PRIMARY KEY,
+  `name` varchar(80),
+  `linkIco` varchar(255)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`streetArt_id`) REFERENCES `streetArt` (`id`);
+ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`);
