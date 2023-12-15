b876b819626038e0ccaa8f8340dd9a3b9d952efc
techmefr <compigni.gaetan@gmail.com>
2023-12-15 09:39:36 +0100

    correction apres relecture

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,25 +1,23 @@
 CREATE DATABASE IF NOT EXISTS `artAndGo`
-CHARACTER SET utf8mb4 COLLATE
-utf8mb4_unicode_ci;
 USE artAndGo;
 
 CREATE TABLE `streetArt` (
   `id` int PRIMARY KEY,
   `name` varchar(80),
   `linkImage` varchar(255)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+);
 
 CREATE TABLE `streetArt_Theme` (
   `id` int PRIMARY KEY,
   `streetArt_id` int,
   `theme_id` int
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+);
+ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`streetArt_id`) REFERENCES `streetArt` (`id`);
+ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`);
 
 CREATE TABLE `theme` (
   `id` int PRIMARY KEY,
   `name` varchar(80),
-  `linkIco` varchar(255)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+  `linkImage` varchar(255)
+);
 
-ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`streetArt_id`) REFERENCES `streetArt` (`id`);
-ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`);
