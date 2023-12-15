d32dd04813869158745f4df6f3d7b9152ae672b1
hamed-cell <attar30pro@gmail.com>
2023-12-15 11:16:00 +0100

    oeuvre Main home.jsx

---
+++ frontend/src/components/Oeuvre.jsx
@@ -0,0 +1,63 @@
+// eslint-disable-next-line import/no-unresolved
+import "../style/style.css";
+
+// eslint-disable-next-line import/prefer-default-export
+export function Oeuvres() {
+  return (
+    <div className="oeuvres">
+      <div className="overlap">
+        <div className="frame">
+          <img
+            className="street-art"
+            alt="Street art"
+            src="street-art-8bit-photo-1-1.png"
+          />
+          <img
+            className="street-art-bit"
+            alt="Street art"
+            src="street-art-8bit-photo-1-2.png"
+          />
+          <div className="text-wrapper">Cyber Coeur</div>
+          <div className="overlap-group">
+            <div className="group" />
+            <img className="rectangle" alt="Rectangle" src="rectangle-10.svg" />
+            <div className="div" />
+            <img className="logo" alt="Logo" src="logo-2.png" />
+            <img
+              className="art-modified"
+              alt="Art modified"
+              src="art-modified-1.png"
+            />
+          </div>
+          <img
+            className="img"
+            alt="Street art"
+            src="street-art-8bit-photo-1-5.png"
+          />
+          <img className="rectangle-2" alt="Rectangle" src="rectangle-12.png" />
+          <img
+            className="street-art-2"
+            alt="Street art"
+            src="street-art-8bit-photo-1-3.png"
+          />
+          <img
+            className="street-art-3"
+            alt="Street art"
+            src="street-art-8bit-photo-1-4.png"
+          />
+          <img
+            className="street-art-4"
+            alt="Street art"
+            src="street-art-8bit-photo-1-6.png"
+          />
+          <div className="text-wrapper-2">Art and Go</div>
+        </div>
+        <div className="text-wrapper-3">Hop birds</div>
+        <div className="text-wrapper-4">Light band</div>
+        <div className="text-wrapper-5">8bit</div>
+        <div className="text-wrapper-6">Mon’art</div>
+        <div className="tha-la-route">Thaï la la route</div>
+      </div>
+    </div>
+  );
+}
