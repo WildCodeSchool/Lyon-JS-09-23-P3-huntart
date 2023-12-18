db5dbc9f9054e77acf7860c59c1c5d60996af4b7
techmefr <compigni.gaetan@gmail.com>
2023-12-18 07:58:49 +0100

    correct the order to resolve the conflict

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,7 +1,13 @@
-CREATE DATABASE IF NOT EXISTS `artAndGo`
-USE artAndGo;
+CREATE DATABASE IF NOT EXISTS `artAndGo`;
+USE `artAndGo`;
 
 CREATE TABLE `streetArt` (
+  `id` int PRIMARY KEY,
+  `name` varchar(80),
+  `linkImage` varchar(255)
+);
+
+CREATE TABLE `theme` (
   `id` int PRIMARY KEY,
   `name` varchar(80),
   `linkImage` varchar(255)
@@ -10,14 +16,7 @@ CREATE TABLE `streetArt` (
 CREATE TABLE `streetArt_Theme` (
   `id` int PRIMARY KEY,
   `streetArt_id` int,
-  `theme_id` int
+  `theme_id` int,
+  FOREIGN KEY (`streetArt_id`) REFERENCES `streetArt` (`id`),
+  FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`)
 );
-ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`streetArt_id`) REFERENCES `streetArt` (`id`);
-ALTER TABLE `streetArt_Theme` ADD FOREIGN KEY (`theme_id`) REFERENCES `theme` (`id`);
-
-CREATE TABLE `theme` (
-  `id` int PRIMARY KEY,
-  `name` varchar(80),
-  `linkImage` varchar(255)
-);
-
