7cdfff4b523f20bfae7cda070e2fc9e116ba788d
techmefr <compigni.gaetan@gmail.com>
2023-12-19 15:14:33 +0100

    add router, controller and manager

--- backend/src/router.js
+++ backend/src/router.js
@@ -8,15 +8,19 @@ const router = express.Router();
 
 // Import itemControllers module for handling item-related operations
 const itemControllers = require("./controllers/itemControllers");
+const StreetArtControllers = require("./controllers/streetArtControllers");
 
 // Route to get a list of items
 router.get("/items", itemControllers.browse);
+router.get("./streetArt", StreetArtControllers.browse);
 
 // Route to get a specific item by ID
 router.get("/items/:id", itemControllers.read);
+router.get("/streetArt/:id", StreetArtControllers.read);
 
 // Route to add a new item
 router.post("/items", itemControllers.add);
+router.post("/streetArt", StreetArtControllers.add);
 
 /* ************************************************************************* */
 
