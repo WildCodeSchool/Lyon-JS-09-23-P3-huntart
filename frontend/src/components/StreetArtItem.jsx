3ab188e023344a8c13cfb267fd48886d9f09fd7b
techmefr <compigni.gaetan@gmail.com>
2023-12-20 10:54:22 +0100

    fix

--- frontend/src/components/StreetArtItem.jsx
+++ frontend/src/components/StreetArtItem.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 function StreetArtItem({ id, name, url }) {
   return (
     <div className="street-art-item">
-      <p>coucou</p>
       <img src={url} alt={`StreetArt ${id}`} />
       <p>{name}</p>
     </div>
