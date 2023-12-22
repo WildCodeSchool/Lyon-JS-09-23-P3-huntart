a1cc305ff6837ae5b6b9c0b112e7ebca9c600228
techmefr <compigni.gaetan@gmail.com>
2023-12-22 10:45:08 +0100

    fix seed

--- backend/seed.js
+++ backend/seed.js
@@ -1,44 +1,25 @@
-/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-
-// Load environment variables from .env file
 require("dotenv").config();
 
-// Import Faker library for generating fake data
-const { faker } = require("@faker-js/faker");
-
-// Import database client
 const database = require("./database/client");
 
 const seed = async () => {
   try {
-    // Declare an array to store the query promises
-    // See why here: https://eslint.org/docs/latest/rules/no-await-in-loop
     const queries = [];
 
-    /* ************************************************************************* */
-
-    // Generating Seed Data
-
-    // Optional: Truncate tables (remove existing data)
     await database.query("TRUNCATE streetArt");
-
-    // Insert fake data into the 'item' table
 
     for (let i = 1; i < 13; i += 1) {
       queries.push(
         database.query("INSERT INTO streetArt(name, linkImage) VALUES (?, ?)", [
-          faker.lorem.word(),
-          `/assets/oeuvres/sta${i.toString()}.jpg`,
+          i + 1,
+          `Street Art ${i + 1}`,
+          `/assets/oeuvres/sta${i}.jpg`,
         ])
       );
     }
 
-    /* ************************************************************************* */
-
-    // Wait for all the insertion queries to complete
     await Promise.all(queries);
 
-    // Close the database connection
     database.end();
 
     console.info(`${database.databaseName} filled from ${__filename} 🌱`);
@@ -47,5 +28,4 @@ const seed = async () => {
   }
 };
 
-// Run the seed function
 seed();
